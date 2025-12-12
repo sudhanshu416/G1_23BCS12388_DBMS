9d9c846e53a610f31ba9b022a42108d435d21f49
Sudhanshu <68190205+sudhanshu416@users.noreply.github.com>
2025-12-12 20:49:44 +0530

    Create S6.sql

---
+++ G1_23BCS12388_Session6/S6.sql
@@ -0,0 +1,22 @@
+-- Write your PostgreSQL query statement below
+
+(SELECT name as results
+FROM
+    (SELECT user_id, name, COUNT(*) rating_cnt
+    FROM MovieRating LEFT JOIN Users USING (user_id)
+    GROUP BY user_id, name) users_
+ORDER BY rating_cnt DESC, name
+LIMIT 1)
+
+UNION ALL
+
+(SELECT title as results
+FROM
+    (SELECT movie_id, title, AVG(rating) avg_rating
+    FROM MovieRating LEFT JOIN Movies USING (movie_id)
+    WHERE to_char(created_at, 'YYYY-MM') = '2020-02'
+    GROUP BY movie_id, title) movies_
+ORDER BY avg_rating DESC, title
+LIMIT 1)
+
+-- G1_23BCS12388_Sudhanshu_Session6
