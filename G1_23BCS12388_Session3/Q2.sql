0cfcf8d3a06dc8aede5d906eff6acb511d6c3e4f
Sudhanshu <68190205+sudhanshu416@users.noreply.github.com>
2025-12-12 20:37:09 +0530

    Create Q2.sql

---
+++ G1_23BCS12388_Session3/Q2.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT Visits.customer_id, COUNT(Visits.visit_id) as count_no_trans
+FROM Visits
+LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id
+WHERE transaction_id IS NULL
+GROUP BY customer_id
+
+-- G1_23BCS12388_Sudhanshu_Session3
